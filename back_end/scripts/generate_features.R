3524258720d0fec63f0ae075283aacb5917240de
Daniel Loos <daniel.loos@hki-jena.de>
2021-01-14 16:19:41 +0100

    Add comment: compositions version

--- back_end/scripts/generate_features.R
+++ back_end/scripts/generate_features.R
@@ -185,6 +185,7 @@ norm_table <- function(abundance_tbl, normalization_method, prevalent_taxa, remo
     "clr" = {
       abundance_tbl %>%
         dplyr::group_by(sample_id) %>%
+        # compositions version 1.40.3 needed. Subscript out of bounds error otherwise
         dplyr::mutate(abundance = compositions::clr(abundance))
     },
     "raw" = {
