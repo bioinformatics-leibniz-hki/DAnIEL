cb19c7f45f140312a6bf1b6d2b4b4704dabeca55
Daniel Loos <daniel.loos@hki-jena.de>
2021-04-15 10:32:56 +0200

    Update deploy.sh: Specify tag latest

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@
 export COMPOSE_DOCKER_CLI_BUILD=1 
 export DOCKER_BUILDKIT=1
 
-docker pull bioinformaticsleibnizhki/daniel_backend
-docker pull bioinformaticsleibnizhki/daniel_frontend
+docker pull bioinformaticsleibnizhki/daniel_backend:latest
+docker pull bioinformaticsleibnizhki/daniel_frontend:latest
 
 docker-compose build --parallel
 docker-compose up
